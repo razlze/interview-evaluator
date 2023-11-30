022ef6d1eaef04713baba848c5d3bab600d0d61e
Razi Syed <r33syed@uwaterloo.ca>
2023-11-30 11:40:31 -0500

    use edge runtime to speechToText

--- app/util/speechToText/route.jsx
+++ app/util/speechToText/route.jsx
@@ -1,16 +1,20 @@
 import OpenAI from 'openai';
 import { NextResponse } from 'next/server';
-import * as fs from 'node:fs';
+// import * as fs from 'node:fs';
 
 const openai = new OpenAI();
+
+export const runtime = 'edge';
 
 export async function POST(request) {
   const blob = await request.blob();
 
-  fs.writeFileSync('/tmp/audio.mp3', Buffer.from(await blob.arrayBuffer()));
+  // fs.writeFileSync('/tmp/audio.mp3', Buffer.from(await blob.arrayBuffer()));
+  const audio = new File([blob], 'answer.webm');
 
   const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream('/tmp/audio.mp3'),
+    // file: fs.createReadStream('/tmp/audio.mp3'),
+    file: audio,
     model: 'whisper-1',
   });
 
